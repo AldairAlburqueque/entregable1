a142c0474af851f2eddb130e8c6f4b5f7b3e2a13
AldairAlburqueque <aldairalburqueque8@gmail.com>
2023-01-22 21:49:36 -0500

    actualizado

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Add = ({colorRandom}) => {
   return (
     <div style={{color:colorRandom}} className="enlaces">
-        <p>Este es mi repositorio <a href="https://github.com/AldairAlburqueque/entregable1.git" target="_blank" >Git </a> y este es mi desplegable de  <a href="https://www.facebook.com/" target="_blank">Netlify</a> </p>
+        <p>Este es mi repositorio <a href="https://github.com/AldairAlburqueque/entregable1.git" target="_blank" >Git </a> y este es mi desplegable de  <a href="entregable1-aldair-alburqueque.netlify.app" target="_blank">Netlify</a> </p>
     </div>
   )
 }
