7590a34e010fd3739960698848c9bcf47a2ce8d9
AldairAlburqueque <aldairalburqueque8@gmail.com>
2023-01-22 21:36:56 -0500

    add

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Add = ({colorRandom}) => {
   return (
     <div style={{color:colorRandom}} className="enlaces">
-        <p>Este es mi repositorio <a href="https://www.youtube.com/" target="_blank" >Git </a> y este es mi desplegable de  <a href="https://www.facebook.com/" target="_blank">Netifly</a> </p>
+        <p>Este es mi repositorio <a href="https://github.com/AldairAlburqueque/entregable1.git" target="_blank" >Git </a> y este es mi desplegable de  <a href="https://www.facebook.com/" target="_blank">Netifly</a> </p>
     </div>
   )
 }
